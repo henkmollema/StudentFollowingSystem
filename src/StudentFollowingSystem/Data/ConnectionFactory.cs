a5babb9f16716e1a5561e86e1c422167a312568b
Henk Mollema <henkmollema@gmail.com>
2015-02-06 14:04:49 +0100

    Added ConnectionFactory and StudentRepository - #1

---
+++ src/StudentFollowingSystem/Data/ConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace StudentFollowingSystem.Data
+{
+    public static class ConnectionFactory
+    {
+        public static IDbConnection GetOpenConnection()
+        {
+            // todo: add connection string.
+            var con = new SqlConnection();
+            con.Open();
+            return con;
+        }
+    }
+}
---
+++ src/StudentFollowingSystem/Data/Repositories/StudentRepository.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dommel;
+using StudentFollowingSystem.Models;
+
+namespace StudentFollowingSystem.Data.Repositories
+{
+    public class StudentRepository
+    {
+        public List<Student> GetAll()
+        {
+            using (var con = ConnectionFactory.GetOpenConnection())
+            {
+                return con.GetAll<Student>().ToList();
+            }
+        }
+
+        public Student GetById(int id)
+        {
+            using (var con = ConnectionFactory.GetOpenConnection())
+            {
+                return con.Get<Student>(id);
+            }
+        }
+
+        public Student GetByEmail(string email)
+        {
+            using (var con = ConnectionFactory.GetOpenConnection())
+            {
+                return con.Select<Student>(s => s.Email == email).FirstOrDefault();
+            }
+        }
+
+        public int Add(Student student)
+        {
+            using (var con = ConnectionFactory.GetOpenConnection())
+            {
+                return con.Insert(student);
+            }
+        }
+
+        public bool Update(Student student)
+        {
+            using (var con = ConnectionFactory.GetOpenConnection())
+            {
+                return con.Update(student);
+            }
+        }
+
+        public void Delete(Student student)
+        {
+            using (var con = ConnectionFactory.GetOpenConnection())
+            {
+                con.Delete(student);
+            }
+        }
+    }
+}

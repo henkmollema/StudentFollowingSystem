45cec4deeee2238a5ec08cfe9690a071905a945c
Henk Mollema <henkmollema@gmail.com>
2015-02-06 15:33:43 +0100

    Validation for login model.

--- src/StudentFollowingSystem/ViewModels/LoginModel.cs
+++ src/StudentFollowingSystem/ViewModels/LoginModel.cs
@@ -1,9 +1,16 @@
-﻿namespace StudentFollowingSystem.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentFollowingSystem.ViewModels
 {
     public class LoginModel
     {
+        [Required(ErrorMessage = "Vul een e-mailadres in.")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Vul een geldig e-mailadres in.")]
+        [EmailAddress(ErrorMessage = "Vul een geldig e-mailadres in.")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Vul een wachtwoord in.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }

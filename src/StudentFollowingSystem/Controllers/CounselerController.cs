6ddf859902f7f5fdeaca88dcada671795f46d6ff
Henk Mollema <henkmollema@gmail.com>
2015-02-06 14:37:26 +0100

    Added counseler controller with basic login logic - #7.

---
+++ src/StudentFollowingSystem/Controllers/CounselerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Security;
+using StudentFollowingSystem.Data.Repositories;
+using StudentFollowingSystem.ViewModels;
+
+namespace StudentFollowingSystem.Controllers
+{
+    public class CounselerController : Controller
+    {
+        private readonly CounselerRepository _counselerRepository = new CounselerRepository();
+
+        public ActionResult Login()
+        {
+            return View(new LoginModel());
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var counseler = _counselerRepository.GetByEmail(model.Email);
+                if (counseler != null)
+                {
+                    if (Crypto.VerifyHashedPassword(counseler.Password, model.Password))
+                    {
+                        FormsAuthentication.SetAuthCookie(counseler.Email, true);
+                        return RedirectToAction("Dashboard");
+                    }
+                }
+
+                ModelState.AddModelError("", "Je gebruikersnaam of wachtwoord klopt niet");
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Dashboard()
+        {
+            var model = new DashboardModel();
+            return View(model);
+        }
+    }
+}
---
+++ src/StudentFollowingSystem/ViewModels/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentFollowingSystem.ViewModels
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}

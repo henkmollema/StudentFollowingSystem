2cda1560b992f943dcf0720ef6660b296f4497f0
Henk Mollema <henkmollema@gmail.com>
2015-02-17 15:21:33 +0100

    Added Class domain model - fixes #13.

---
+++ src/StudentFollowingSystem/Models/Class.cs
@@ -0,0 +1,15 @@
+﻿namespace StudentFollowingSystem.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Section { get; set; }
+
+        public int CounselerId { get; set; }
+
+        public Counseler Counseler { get; set; }
+    }
+}

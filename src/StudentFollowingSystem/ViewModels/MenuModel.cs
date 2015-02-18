16cff00d51b92eb38e57586c328fb100c4ebf9fd
WesselKoorn <wesselkoorn@icloud.com>
2015-02-18 12:43:40 +0100

    Create partial view for menu with login check.

---
+++ src/StudentFollowingSystem/ViewModels/MenuModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentFollowingSystem.ViewModels
+{
+    public class MenuModel
+    {
+        public bool IsStudent { get; set; }
+        public bool IsCounseler { get; set; }
+    }
+}

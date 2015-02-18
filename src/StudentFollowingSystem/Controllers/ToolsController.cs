742c43222cdaf309ba6128c59843e84f02276725
Henk Mollema <henkmollema@gmail.com>
2015-02-18 13:38:07 +0100

    Added password hasher in tools controller - fixes #16.

---
+++ src/StudentFollowingSystem/Controllers/ToolsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace StudentFollowingSystem.Controllers
+{
+    public class ToolsController : Controller
+    {
+        public ActionResult HashPassword(string q)
+        {
+            string password = Crypto.HashPassword(q);
+            return Content(password);
+        }
+    }
+}

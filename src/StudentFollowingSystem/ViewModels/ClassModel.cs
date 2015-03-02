0d93c61a76df0011cfda88673753a7dbf4056a95
Henk Mollema <henkmollema@gmail.com>
2015-03-02 19:52:11 +0100

    Added data annotations on class model - #21.

--- src/StudentFollowingSystem/ViewModels/ClassModel.cs
+++ src/StudentFollowingSystem/ViewModels/ClassModel.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Web.Mvc;
 
 namespace StudentFollowingSystem.ViewModels
 {
     public class ClassModel
     {
+        [Display(Name = "Klas naam")]
+        [Required(ErrorMessage = "Naam is verplicht")]
         public string Name { get; set; }
 
+        [Display(Name = "Afdeling")]
         public string Section { get; set; }
 
+        [Display(Name = "SLB'er")]
+        [Required(ErrorMessage = "SLB'er is verplicht")]
         public int CounselerId { get; set; }
 
         public CounselerModel Counseler { get; set; }

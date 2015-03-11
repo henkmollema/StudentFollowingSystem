2c1c8a4cc61f709bedf8b0b1e6f546c0b2462975
Tim <timkuiper02@hotmail.com>
2015-03-11 13:17:52 +0100

    Buttons and latest pages translated, fixes #31

--- src/StudentFollowingSystem/ViewModels/SubjectModel.cs
+++ src/StudentFollowingSystem/ViewModels/SubjectModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,10 +10,13 @@ namespace StudentFollowingSystem.ViewModels
     {
         public int Id { get; set; }
 
+        [Display(Name = "Vak naam")]
         public string Name { get; set; }
 
+        [Display(Name = "Start datum")]
         public DateTime StartDate { get; set; }
 
+        [Display(Name = "Eind datum")]
         public DateTime EndDate { get; set; }
     }
 }

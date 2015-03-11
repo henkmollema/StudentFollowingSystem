7b784ca8bb5142f68223c387b063fcbd0bde8d8d
Henk Mollema <henkmollema@gmail.com>
2015-03-11 13:18:39 +0100

    Start and end date validation for subjects - #37.

--- src/StudentFollowingSystem/ViewModels/SubjectModel.cs
+++ src/StudentFollowingSystem/ViewModels/SubjectModel.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace StudentFollowingSystem.ViewModels
 {
-    public class SubjectModel
+    public class SubjectModel : IValidatableObject
     {
         public int Id { get; set; }
 
@@ -14,5 +13,14 @@ namespace StudentFollowingSystem.ViewModels
         public DateTime StartDate { get; set; }
 
         public DateTime EndDate { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (StartDate > EndDate)
+            {
+                // Start date is after end date.
+                yield return new ValidationResult("De start datum/tijd kan niet na de eind datum/tijd liggen.");
+            }
+        }
     }
 }

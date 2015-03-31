98f835dda159901485e40e56e478903f35506efb
WesselKoorn <WesselKoorn@icloud.com>
2015-03-31 15:05:50 +0200

    Added Counselings table to project

---
+++ src/StudentFollowingSystem.Database/dbo/Tables/Counselings.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Counselings] (
+    [Id]            INT  IDENTITY (1, 1) NOT NULL,
+    [AppointmentId] INT  NOT NULL,
+    [Comment]       TEXT NULL,
+    [Private]       BIT  DEFAULT ((0)) NOT NULL,
+    [Status]        INT  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+

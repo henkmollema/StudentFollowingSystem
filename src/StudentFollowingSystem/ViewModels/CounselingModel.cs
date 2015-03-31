1d0ae69f2e65e3bf7878c6036dcc4dea829bdfc0
Tim <timkuiper02@hotmail.com>
2015-03-31 15:26:06 +0200

    Added validations to counseling create

--- src/StudentFollowingSystem/ViewModels/CounselingModel.cs
+++ src/StudentFollowingSystem/ViewModels/CounselingModel.cs
@@ -12,12 +12,17 @@ namespace StudentFollowingSystem.ViewModels
 
         public Appointment Appointment { get; set; }
 
+        [Display(Name = "Commentaar")]
+        [Required(ErrorMessage = "Vul commentaar in.")]
         public string Comment { get; set; }
 
+        [Display(Name = "Privé")]
         public bool Private { get; set; }
 
         public Status Status { get; set; }
 
+        [Required(ErrorMessage = "Een nieuwe datum is verplicht. (")]
+        [Display(Name = "Volgende afspraak")]
         public DateTime? NextAppointment { get; set; }
 
         public DateTime AppointmentDate { get; set; }

5d81033cc47737feb24998e62f3a307a7de27d94
Henk Mollema <henkmollema@gmail.com>
2015-04-01 09:14:49 +0200

    Join subjects with classes.

--- src/StudentFollowingSystem/Data/Repositories/SubjectRepository.cs
+++ src/StudentFollowingSystem/Data/Repositories/SubjectRepository.cs
@@ -1,8 +1,43 @@
-﻿using StudentFollowingSystem.Models;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dommel;
+using StudentFollowingSystem.Models;
 
 namespace StudentFollowingSystem.Data.Repositories
 {
     public class SubjectRepository : RepositoryBase<Subject>
     {
+        /// <summary>
+        /// Gets all the subjects joined with the class.
+        /// </summary>
+        public override List<Subject> GetAll()
+        {
+            using (var con = ConnectionFactory.GetOpenConnection())
+            {
+                return con.GetAll<Subject, Class, Subject>(
+                    (s, c) =>
+                    {
+                        s.Class = c;
+                        return s;
+                    }).ToList();
+            }
+        }
+
+        /// <summary>
+        /// Gets a subject joined with the class.
+        /// </summary>
+        /// <param name="id">The id of the subject.</param>
+        public override Subject GetById(int id)
+        {
+            using (var con = ConnectionFactory.GetOpenConnection())
+            {
+                con.Get<Subject, Class, Subject>(id,
+                    (s, c) =>
+                    {
+                        s.Class = c;
+                        return s;
+                    });
+            }
+        }
     }
 }
